7e7bcdc4680cd90fdecdbbab1b4001e40cd3acc8
milenays <milenaystore@gmail.com>
2024-05-15 15:47:08 +0300

    asd

--- backend/server.js
+++ backend/server.js
@@ -1,45 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const cors = require('cors');
-const dotenv = require('dotenv');
-const productRoutes = require('./routes/productRoutes');
-const brandRoutes = require('./routes/brandRoutes');
-const categoryRoutes = require('./routes/categoryRoutes');
-const tagRoutes = require('./routes/tagRoutes');
-const taxRoutes = require('./routes/taxRoutes');
-const variantRoutes = require('./routes/variantRoutes');
-
-// Environment Variables
-dotenv.config();
+const productRoutes = require('./routes/productRoutes'); // API rotaları
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 // Middleware
+app.use(bodyParser.json());
 app.use(cors());
-app.use(express.json());
 
-// Routes
-app.use('/api/products', productRoutes);
-app.use('/api/brands', brandRoutes);
-app.use('/api/categories', categoryRoutes);
-app.use('/api/tags', tagRoutes);
-app.use('/api/taxes', taxRoutes);
-app.use('/api/variants', variantRoutes);
-
-// Database Connection
-const dbURI = process.env.MONGO_URI || 'mongodb://localhost/stockie';
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
-
-// Error Handling Middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
+// Veritabanı bağlantısı
+mongoose.connect('mongodb://localhost:27017/stockie', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+}).then(() => {
+  console.log('MongoDB connected...');
+}).catch(err => {
+  console.log(err);
 });
 
-// Starting the Server
-const PORT = process.env.PORT || 5000;
+// Rotalar
+app.use('/api/products', productRoutes); // Ürün rotaları
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
