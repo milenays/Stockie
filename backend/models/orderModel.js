b6f259a191a9ad689516403a1e3bea64db893fb7
milenays <milenaystore@gmail.com>
2024-05-18 15:24:08 +0300

    asdfasdf

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -1,24 +1,31 @@
 const mongoose = require('mongoose');
 
-const orderLineSchema = new mongoose.Schema({
-  quantity: { type: Number, required: true },
-  productName: { type: String, required: true }
-});
-
 const orderSchema = new mongoose.Schema({
-  orderId: { type: Number, required: true, unique: true },
-  orderNumber: { type: String, required: true },
-  customerName: { type: String, required: true },
-  address: { type: String, required: true },
-  city: { type: String, required: true },
-  district: { type: String, required: true },
-  totalPrice: { type: Number, required: true },
-  cargoTrackingNumber: { type: String, required: true },
-  cargoProviderName: { type: String, required: true },
-  status: { type: String, required: true },
-  orderLines: [orderLineSchema]
+  orderNumber: String,
+  customerFirstName: String,
+  customerLastName: String,
+  totalPrice: Number,
+  currencyCode: String,
+  status: String,
+  shipmentAddress: {
+    fullName: String,
+    address1: String,
+    address2: String,
+    city: String,
+    district: String,
+    postalCode: String,
+    countryCode: String,
+    phone: String,
+  },
+  lines: [
+    {
+      productName: String,
+      quantity: Number,
+      price: Number,
+      productCode: String,
+    },
+  ],
+  orderDate: Date,
 });
 
-const Order = mongoose.model('Order', orderSchema);
-
-module.exports = Order;
+module.exports = mongoose.model('Order', orderSchema);
